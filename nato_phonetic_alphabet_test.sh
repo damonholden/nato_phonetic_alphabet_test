10ac5d4387ef1cc0ba32ceabc6c457c15242ddac
damonholden <damonh003@gmail.com>
2025-10-26 15:10:36 +0000

    add full nato phonetic alphabet to bash file

--- nato_phonetic_alphabet_test.sh
+++ nato_phonetic_alphabet_test.sh
@@ -1,1 +1,32 @@
-echo "hello world"
+nato_phonetic_alphabet=(
+  "Alfa"
+  "Bravo"
+  "Charlie"
+  "Delta"
+  "Echo"
+  "Foxtrot"
+  "Golf"
+  "Hotel"
+  "India"
+  "Juliett"
+  "Kilo"
+  "Lima"
+  "Mike"
+  "November"
+  "Oscar"
+  "Papa"
+  "Quebec"
+  "Romeo"
+  "Sierra"
+  "Tango"
+  "Uniform"
+  "Victor"
+  "Whiskey"
+  "X-ray"
+  "Yankee"
+  "Zulu"
+)
+
+
+
+echo "${nato_phonetic_alphabet[*]}"
