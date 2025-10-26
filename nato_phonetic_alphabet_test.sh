1c9d69bde98240d3ee21f68866be6fe9497c3695
damonholden <damonh003@gmail.com>
2025-10-26 15:40:23 +0000

    implement first functioning script quiz

--- nato_phonetic_alphabet_test.sh
+++ nato_phonetic_alphabet_test.sh
@@ -30,5 +30,18 @@ nato_phonetic_alphabet=(
 nato_phonetic_alphabet_shuffled=$(shuf --echo ${nato_phonetic_alphabet[@]})
 
 for word in ${nato_phonetic_alphabet_shuffled[*]}; do
-    echo $word
+    first_char=${word:0:1}
+    echo $first_char
+    
+    read guessed_word
+    
+    lowercase_guessed_word=${guessed_word,,}
+    lowercase_word=${word,,}
+
+    if [[ "$lowercase_guessed_word" == "$lowercase_word" ]]; then
+        echo "Correct!"
+    else 
+        echo "Wrong!"
+    fi;
+
 done;
