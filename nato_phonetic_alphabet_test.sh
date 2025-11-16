e7f0a21ee0895674fe870c843994f0ed7dcdf576
damonholden <damonh003@gmail.com>
2025-11-16 00:46:32 +0000

    improve output of test

--- nato_phonetic_alphabet_test.sh
+++ nato_phonetic_alphabet_test.sh
@@ -31,17 +31,16 @@ nato_phonetic_alphabet_shuffled=$(shuf --echo ${nato_phonetic_alphabet[@]})
 
 for word in ${nato_phonetic_alphabet_shuffled[*]}; do
     first_char=${word:0:1}
-    echo $first_char
     
-    read guessed_word
+    read -p "$first_char: " guessed_word
     
     lowercase_guessed_word=${guessed_word,,}
     lowercase_word=${word,,}
 
     if [[ "$lowercase_guessed_word" == "$lowercase_word" ]]; then
-        echo "Correct!"
+        printf "    ✅ Correct!\n\n"
     else 
-        echo "Wrong!"
+        printf "    ❌ Wrong!\n\n"
     fi;
 
 done;
